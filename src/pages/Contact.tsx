f970125fa26b9c21eb4470940f55c57753182c3b
Vicente <nunithefanoftheyogscast@gmail.com>
2025-06-02 15:26:11 -0600

    dev link

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import {} from "@heroicons/react/24/solid";
+import devLogo from "/icons/dev-to.svg";
 
 function Contact() {
   const { t } = useTranslation();
@@ -35,6 +36,14 @@ function Contact() {
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                 </svg>
                 LinkedIn
+              </a>
+              <a
+                className="flex gap-2 p-2 text-base sm:text-lg hover:bg-gray-700 items-center rounded"
+                href="https://dev.to/vicentevalenzuelamartinez"
+                target="_blank"
+              >
+                <img src={devLogo} className="w-5 h-5" alt="Vite logo" />
+                DEV Community
               </a>
             </div>
           </div>
